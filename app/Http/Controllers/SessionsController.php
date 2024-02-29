3495f804b828d150138010a170c8d98e47ded6f7
itscharlesm <itscharlesm@gmail.com>
2024-02-29 21:47:46 +0800

    7d

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Anhskohbo\NoCaptcha\Facades\NoCaptcha;
 
 
 class SessionsController extends Controller
@@ -16,26 +17,30 @@ class SessionsController extends Controller
     public function store()
     {
         $attributes = request()->validate([
-            'email'=>'required|email',
-            'password'=>'required' 
+            'email' => 'required|email',
+            'password' => 'required',
+            'g-recaptcha-response' => 'required|captcha',
+        ], [
+            'g-recaptcha-response.required' => 'Please complete the captcha validation.',
+            'g-recaptcha-response.captcha' => 'Captcha validation failed, please try again.',
         ]);
 
-        if(Auth::attempt($attributes))
-        {
-            session()->regenerate();
-            return redirect('dashboard')->with(['success'=>'You are logged in.']);
-        }
-        else{
+        // Remove the 'g-recaptcha-response' from the attributes
+        unset($attributes['g-recaptcha-response']);
 
-            return back()->withErrors(['email'=>'Email or password invalid.']);
+        if (Auth::attempt($attributes)) {
+            session()->regenerate();
+            return redirect('dashboard')->with(['success' => 'You are logged in.']);
+        } else {
+            return back()->withErrors(['email' => 'Email or password invalid.']);
         }
     }
-    
+
     public function destroy()
     {
 
         Auth::logout();
 
-        return redirect('/login')->with(['success'=>'You\'ve been logged out.']);
+        return redirect('/login')->with(['success' => 'You\'ve been logged out.']);
     }
 }
