604fc3f7ef948a08fdee9b765c422ee5c55ea35f
luminarias011 <luminariasmarco@gmail.com>
2024-03-06 16:21:26 +0800

    plieknjsnnhbc uyg

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,101 +4,102 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    @if ($errors->any())
-                        <!-- Error message display -->
-                        <div class="mt-3 alert alert-primary alert-dismissible fade show" role="alert" id="alert-error">
-                            <span class="alert-text text-white">
-                                {{ $errors->first() }}
-                            </span>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @endif
-
-                    @if (session('success'))
-                        <div class="m-3 alert alert-success alert-dismissible fade show" id="alert-success" role="alert">
-                            <span class="alert-text text-white">
-                                {{ session('success') }}
-                            </span>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @elseif(session('error'))
-                    <div class="m-3 alert alert-danger alert-dismissible fade show" id="alert-error" role="alert">
-                        <span class="alert-text text-white">
-                            {{ session('error') }}
-                        </span>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                    @endif
-                </div>
-            </div>
-            <div class="page-header min-height-300 border-radius-xl mt-4"
-                style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%;">
-                <span class="mask bg-gradient-primary opacity-6"></span>
-            </div>
-            <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
-                <div class="row gx-4">
-                    <div class="col-auto">
-                        <div class="avatar avatar-xl position-relative">
-                            <img src="../assets/img/profile-photo.jpg" alt="profile_image"
-                                class="w-100 border-radius-lg shadow-sm">
-                        </div>
-                    </div>
-                    <div class="col-auto my-auto">
-                        <div class="h-100">
-                            <h5 class="mb-1">
-                                {{ $user->first_name }} {{ $user->middle_name }} {{ $user->last_name }}
-                            </h5>
-                            <p class="mb-0 font-weight-bold text-sm">
-                                {{ DB::table('user_roles')->where('id', $user->user_role)->value('user_role') }}
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        @if ($errors->any())
+        <!-- Error message display -->
+        <div class="mt-3 alert alert-primary alert-dismissible fade show" role="alert" id="alert-error">
+          <span class="alert-text text-white">
+            {{ $errors->first() }}
+          </span>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        <div class="container-fluid py-4">
-            <div class="row">
-                <div class="col-12 col-xl-4">
-                    <div class="card h-100">
-                        <div class="card-header pb-0 p-3">
-                            <h6 class="mb-0">Account Settings</h6>
-                            <div class="form-check form-switch mt-3 ps-0">
-                                <input class="form-check-input ms-auto" type="checkbox" id="flexSwitchCheckDefault2"
-                                    checked>
-                                <label class="form-check-label text-body ms-3 text-truncate w-80 mb-0"
-                                    for="flexSwitchCheckDefault2">Email
-                                    me when someone mentions me</label>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="text-uppercase text-body text-xs font-weight-bolder">Account</h6>
-                            <ul class="list-group">
-                                <li class="list-group-item border-0 ps-0 pt-0 text-sm">
-                                    <strong class="text-dark">Role:</strong> &nbsp;
-                                    {{ DB::table('user_roles')->where('id', $user->user_role)->value('user_role') }}
-                                </li>
+        @endif
+
+        @if (session('success'))
+        <div class="m-3 alert alert-success alert-dismissible fade show" id="alert-success" role="alert">
+          <span class="alert-text text-white">
+            {{ session('success') }}
+          </span>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @elseif(session('error'))
+        <div class="m-3 alert alert-danger alert-dismissible fade show" id="alert-error" role="alert">
+          <span class="alert-text text-white">
+            {{ session('error') }}
+          </span>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+      </div>
+    </div>
+    <div class="page-header min-height-300 border-radius-xl mt-4"
+      style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%;">
+      <span class="mask bg-gradient-primary opacity-6"></span>
+    </div>
+    <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
+      <div class="row gx-4">
+        <div class="col-auto">
+          <div class="avatar avatar-xl position-relative">
+            <img src="../assets/img/profile-photo.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+          </div>
+        </div>
+        <div class="col-auto my-auto">
+          <div class="h-100">
+            <h5 class="mb-1">
+              {{ $user->first_name }} {{ $user->middle_name }} {{ $user->last_name }}
+            </h5>
+            <p class="mb-0 font-weight-bold text-sm">
+              {{ DB::table('user_roles')->where('id', $user->user_role)->value('user_role') }}
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid py-4">
+    <div class="row">
+      <div class="col-12 col-xl-4">
+        <div class="card h-100">
+          <div class="card-header pb-0 p-3">
+            <h6 class="mb-0">Account Settings</h6>
+            <div class="form-check form-switch mt-3 ps-0">
+              <input class="form-check-input ms-auto" type="checkbox" id="flexSwitchCheckDefault2" checked>
+              <label class="form-check-label text-body ms-3 text-truncate w-80 mb-0" for="flexSwitchCheckDefault2">Email
+                me when someone mentions me</label>
+            </div>
+          </div>
+          <div class="card-body">
+            <h6 class="text-uppercase text-body text-xs font-weight-bolder">Account</h6>
+            <ul class="list-group">
+              <li class="list-group-item border-0 ps-0 pt-0 text-sm">
+                <strong class="text-dark">Role:</strong> &nbsp;
+                {{ DB::table('user_roles')->where('id', $user->user_role)->value('user_role') }}
+              </li>
 
 
-                                <li class="list-group-item border-0 ps-0 pt-0 text-sm">
-                                    <strong class="text-dark">Email:</strong> &nbsp; {{ $user->email }}
-                                    <a href="#" class="fas fa-user-edit text-secondary text-sm float-end"  data-bs-toggle="modal" data-bs-target="#editEmailModal" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Email"></a>
+              <li class="list-group-item border-0 ps-0 pt-0 text-sm">
+                <strong class="text-dark">Email:</strong> &nbsp; {{ $user->email }}
+                <a href="#" class="fas fa-user-edit text-secondary text-sm float-end" data-bs-toggle="modal"
+                  data-bs-target="#editEmailModal" data-bs-toggle="tooltip" data-bs-placement="top"
+                  title="Edit Email"></a>
 
-                                </li>
+              </li>
 
-                                <li class="list-group-item border-0 ps-0 pt-0 text-sm">
-                                    <strong class="text-dark">Password:</strong> &nbsp; *******
-                                    <a href="#" class="fas fa-user-edit text-secondary text-sm float-end"  data-bs-toggle="modal" data-bs-target="#editPasswordModal" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Password"></a>
+              <li class="list-group-item border-0 ps-0 pt-0 text-sm">
+                <strong class="text-dark">Password:</strong> &nbsp; *******
+                <a href="#" class="fas fa-user-edit text-secondary text-sm float-end" data-bs-toggle="modal"
+                  data-bs-target="#editPasswordModal" data-bs-toggle="tooltip" data-bs-placement="top"
+                  title="Edit Password"></a>
 
-                                </li>
+              </li>
 
-                                <li class="list-group-item border-0 ps-0 pt-0 text-sm">
-                                    <strong class="text-dark">Date Created:</strong> &nbsp;{{ $user->created_at }}
-                                </li>
+              <li class="list-group-item border-0 ps-0 pt-0 text-sm">
+                <strong class="text-dark">Date Created:</strong> &nbsp;{{ $user->created_at }}
+              </li>
 
               <li class="list-group-item border-0 px-0">
 
@@ -183,14 +184,14 @@
                   }
 
                   @endphp
-                  <p class="mb-0 text-xs">{{ $chat->cht_message }}</p>
+                  <p class="mb-0 text-xs">{{ $decryptedMessage }}</p>
                 </div>
                 <button class="btn btn-link pe-3 ps-0 mb-0 ms-auto" data-bs-toggle="offcanvas"
-                  data-bs-target="#chatModal{{ $chat->cht_to }}" data-chat-to="{{ $chat->cht_to }}">Reply</button>
+                  data-bs-target="#chatModal{{ $chat->cht_from }}" data-chat-to="{{ $chat->cht_from }}">Reply</button>
               </li>
 
               {{-- ! OFF CANVAS LEFT--}}
-              <div class="offcanvas offcanvas-start" tabindex="-1" id="chatModal{{ $chat->cht_to }}"
+              <div class="offcanvas offcanvas-start" tabindex="-1" id="chatModal{{ $chat->cht_from }}"
                 aria-labelledby="offcanvasBothLabel" data-bs-scroll="true">
                 <div class="offcanvas-header">
                   <h5 id="offcanvasBothLabel" class="offcanvas-title">{{ $chat->first_name }} {{ $chat->last_name }}
@@ -206,22 +207,13 @@
                     @csrf
                     <input type="hidden" name="recipient_id" value="{{ $chat->cht_from }}">
                     <div class="d-flex mx-auto">
-                      <input class="form-control me-2 mb-2 d-grid w-100" style="flex-basis: 100%" name="message" id="message"
-                        required></input>
-                      <button type="submit" style="flex-basis: 20%" class="btn btn-primary mb-2 d-grid w-100">Send</button>
+                      <input class="form-control me-2 mb-2 d-grid w-100" style="flex-basis: 100%" name="message"
+                        id="message" required></input>
+                      <button type="submit" style="flex-basis: 20%"
+                        class="btn btn-primary mb-2 d-grid w-100">Send</button>
                     </div>
                   </form>
-                  {{-- @if ($book->bookURL == '#')
-                  <a type="button" class="btn btn-primary mb-2 d-grid w-100" href="{{ $book->bookURL }}">No
-                      review</a>
-                  @else
-                  <a type="button" class="btn btn-primary mb-2 d-grid w-100" target="_blank"
-                      href="{{ $book->bookURL }}">See
-                      review</a>
-                  @endif --}}
-                  {{-- <button type="button" class="btn btn-outline-secondary d-grid w-100"
-                      data-bs-dismiss="offcanvas">Back</button> --}}
-              </div>
+                </div>
               </div>
               {{-- ! --}}
 
@@ -271,65 +263,71 @@
 </div>
 
 <div class="modal fade" id="editEmailModal" tabindex="-1" aria-labelledby="editEmailModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editEmailModalLabel">Edit Email</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- Add your form for editing email here -->
-                <form  method="POST" action="/change_email/">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="newEmail" class="form-label">New Email:</label>
-                        <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Enter new email" required>
-                    </div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editEmailModalLabel">Edit Email</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Add your form for editing email here -->
+        <form method="POST" action="/change_email/">
+          @csrf
+          <div class="mb-3">
+            <label for="newEmail" class="form-label">New Email:</label>
+            <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Enter new email"
+              required>
+          </div>
 
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Enter Password:</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter Current Password" required>
-                    </div>
-                    <!-- Add any other necessary form fields -->
+          <div class="mb-3">
+            <label for="password" class="form-label">Enter Password:</label>
+            <input type="password" class="form-control" id="password" name="password"
+              placeholder="Enter Current Password" required>
+          </div>
+          <!-- Add any other necessary form fields -->
 
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
-                </form>
-            </div>
-        </div>
+          <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
+      </div>
     </div>
+  </div>
 </div>
 
 <!-- Edit Password Modal -->
-<div class="modal fade" id="editPasswordModal" tabindex="-1" aria-labelledby="editPasswordModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editPasswordModalLabel">Edit Password</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- Add your form for editing password here -->
-                <form method="POST" action="/change_password/">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="currentPassword" class="form-label">Current Password:</label>
-                        <input type="password" class="form-control" id="currentPassword" name="currentPassword" placeholder="Enter current password" required>
-                    </div>
+<div class="modal fade" id="editPasswordModal" tabindex="-1" aria-labelledby="editPasswordModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editPasswordModalLabel">Edit Password</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Add your form for editing password here -->
+        <form method="POST" action="/change_password/">
+          @csrf
+          <div class="mb-3">
+            <label for="currentPassword" class="form-label">Current Password:</label>
+            <input type="password" class="form-control" id="currentPassword" name="currentPassword"
+              placeholder="Enter current password" required>
+          </div>
 
-                    <div class="mb-3">
-                        <label for="newPassword" class="form-label">New Password:</label>
-                        <input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Enter new password" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="ConfirmNewPassword" class="form-label">Confirm new Password:</label>
-                        <input type="password" class="form-control" id="ConfirmNewPassword" name="ConfirmNewPassword" placeholder="Confirm new password" required>
-                    </div>
+          <div class="mb-3">
+            <label for="newPassword" class="form-label">New Password:</label>
+            <input type="password" class="form-control" id="newPassword" name="newPassword"
+              placeholder="Enter new password" required>
+          </div>
+          <div class="mb-3">
+            <label for="ConfirmNewPassword" class="form-label">Confirm new Password:</label>
+            <input type="password" class="form-control" id="ConfirmNewPassword" name="ConfirmNewPassword"
+              placeholder="Confirm new password" required>
+          </div>
 
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
-                </form>
-            </div>
-        </div>
+          <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
+      </div>
     </div>
+  </div>
 </div>
 
 
@@ -413,5 +411,5 @@
 
 
 
-    </script>
+</script>
 @endsection
