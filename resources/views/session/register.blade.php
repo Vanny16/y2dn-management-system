95daea05a2962b685324c59e64f6aa8ca2fe610f
vanny16 <evan.bonso12@gmail.com>
2024-05-30 20:09:05 +0800

    on going login

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <section class="min-vh-100 mb-8">
-    <div class="page-header align-items-start min-vh-75 pt-5 pb-11 mx-3 border-radius-lg" style="background-image: url('../assets/img/y2dn.jpg'); background-size: cover;">
+    <div class="page-header align-items-start min-vh-75 pt-5 pb-11 mx-3 border-radius-lg" style="background-image: url('../assets/img/homebg1.jpg'); background-size: cover;">
         <span class="mask bg-gradient-dark opacity-3"></span>
     </div>
 
     <div class="container">
-        <div class="row mt-lg-n10 mt-md-n11 mt-n20 opacity-">
+        <div class="row mt-lg-n10 mt-md-n11 mt-n20" >
             <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                 <div class="card z-index-0">
                     <div class="card-header text-center pt-4">
@@ -29,8 +29,8 @@
                             @csrf
 
                             <div class="mb-3">
-                                <input type="text" class="form-control" placeholder="Enter Username" name="username" id="username" aria-label="Last Name" value="{{ old('username') }}">
-                                @error('username')
+                                <input type="text" class="form-control" placeholder="Enter email" name="email" id="email" aria-label="email" value="{{ old('email') }}">
+                                @error('email')
                                     <p class="text-danger text-xs mt-2">{{ $message }}</p>
                                 @enderror
                             </div>
@@ -41,7 +41,17 @@
                                     <p class="text-danger text-xs mt-2">{{ $message }}</p>
                                 @enderror
                             </div>
-
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                              </div>
+                              <div class="form-group">
+                                {!! NoCaptcha::renderJs() !!}
+                                {!! NoCaptcha::display() !!}
+                                @error('g-recaptcha-response')
+                                    <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                @enderror
+                            </div>
 
                             <div class="text-center">
                               <button type="submit" class="btn bg-gradient-success w-100 my-4 mb-2">Sign In</button>
@@ -52,6 +62,7 @@
             </div>
         </div>
     </div>
+
 </section>
 
 @endsection
